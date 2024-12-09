b8f48a11d5ada491c7cb3e21f89f9ea3fed3e907
Yousef-Sh3ban <wewewe1551@gmail.com>
2024-12-09 15:38:17 +0200

    some edit

--- lib/network/app_end_points.dart
+++ lib/network/app_end_points.dart
@@ -1,6 +1,8 @@
 abstract class AppEndPoints {
   static const String baseUrl = "https://dummyjson.com/";
   
+  static const String notifactionUrl = "https://dummyjson.com/c/e6f8-72fb-4d9a-8202?page=1&limit=10";
+  
   static const String apiKey = "2f4b89b9da7a48889d3615dde4f8bd6f";
   static const String productsSearch = "products";
   static String productsDetails(String id) => "products/$id";
